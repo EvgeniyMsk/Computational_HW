3e62dd500cbaafbd7e2efb66bbbe8bdaf3d12c8d
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2014-10-31 14:24:56 +0400

    interpolation in the middle

--- task3.py
+++ task3.py
@@ -25,6 +25,8 @@ if __name__ == "__main__":
             1.834414,
             1.843768]
 
+    #print zip(xs, f_xs)
+
     m = 5
 
     diffs = []
@@ -61,12 +63,22 @@ if __name__ == "__main__":
     y2 = Symbol('y2')
     y3 = Symbol('y3')
     y4 = Symbol('y4')
-    intr_poly = Poly(y0 + y1 * t + (y2 / 2) * t * (t - 1) + (y3 / 6) * t * (t - 1) * (t - 2)
+
+    intrp1 = Poly(y0 + y1 * t + (y2 / 2) * t * (t - 1) + (y3 / 6) * t * (t - 1) * (t - 2)
                      + (y4 / 24) * t * (t - 1) * (t - 2) * (t - 3))
+
+    intrp2 = Poly(y0 + y1 * t + (y2 / 2) * t * (t + 1) + (y3 / 6) * t * (t + 1) * (t + 2)
+                     + (y4 / 24) * t * (t + 1) * (t + 2) * (t + 3))
+
+    intrp3 = Poly(y0 + y1 * t + (y2 / 2) * t * (t - 1) + (y3 / 6) * t * (t - 1) * (t + 1)
+                     + (y4 / 24) * t * (t - 1) * (t + 1) * (t - 2))
 
 
     x1 = 0.171494
     x2 = 0.764488
+    x3 = 0.408541
+
+    y = 1.817978
 
     sub1 = [(y0, diffs[0][1]),
             (y1, diffs[1][1]),
@@ -82,14 +94,122 @@ if __name__ == "__main__":
             (y4, diffs[4][n - 6]),
             (t,  (x2 - xs[8]) / h)]
 
-    p1 = intr_poly.subs(sub1)
-    p2 = intr_poly.subs(sub2)
+
+    p1 = intrp1.subs(sub1)
+    p2 = intrp2.subs(sub2)
+
 
     print " "
+
     print "       x_1      |       p(x_1)   "
     print "---------------------------------------------------------------------------"
     print "  {:+.9f}  |  {:+.9f}".format(float(x1), float(p1))
     print "---------------------------------------------------------------------------"
+
     print "       x_2      |       p(x_2)   "
     print "---------------------------------------------------------------------------"
     print "  {:+.9f}  |  {:+.9f}".format(float(x2), float(p2))
+    print "---------------------------------------------------------------------------"
+
+
+    ind = 4
+    sub1 = [(y0, diffs[0][ind]),
+            (y1, diffs[1][ind]),
+            (y2, diffs[2][ind]),
+            (y3, diffs[3][ind]),
+            (y4, diffs[4][ind]),
+            (t,  (x3 - xs[ind]) / h)]
+    p3 = intrp1.subs(sub1)
+
+    print
+    print " Table begin "
+    print
+    print "       x_3      |       p(x_3)   "
+    print "---------------------------------------------------------------------------"
+    print "  {:+.9f}  |  {:+.9f}".format(float(x3), float(p3))
+    print "---------------------------------------------------------------------------"
+
+
+    sub3 = [(y0, diffs[0][ind]),
+            (y1, diffs[1][ind]),
+            (y2, diffs[2][ind - 1]),
+            (y3, diffs[3][ind - 1]),
+            (y4, diffs[4][ind - 2]),
+            (t,  (x3 - xs[ind]) / h)]
+
+    p3 = intrp3.subs(sub3)
+
+    print
+    print " Table middle "
+    print
+    print "       x_3      |       p(x_3)   "
+    print "---------------------------------------------------------------------------"
+    print "  {:+.9f}  |  {:+.9f}".format(float(x3), float(p3))
+    print "---------------------------------------------------------------------------"
+
+    sub3 = [(y0, diffs[0][n - ind - 1]),
+            (y1, diffs[1][n - ind - 2]),
+            (y2, diffs[2][n - ind - 3]),
+            (y3, diffs[3][n - ind - 4]),
+            (y4, diffs[4][n - ind - 5]),
+            (t,  (x3 - xs[n - ind - 1]) / h)]
+
+    p3 = intrp2.subs(sub3)
+
+    print
+    print " Table end "
+    print
+    print "       x_3      |       p(x_3)   "
+    print "---------------------------------------------------------------------------"
+    print "  {:+.9f}  |  {:+.9f}".format(float(x3), float(p3))
+    print "---------------------------------------------------------------------------"
+
+    ind = 6
+    sub4 = [(y0, diffs[0][ind]),
+            (y1, diffs[1][ind]),
+            (y2, diffs[2][ind - 1]),
+            (y3, diffs[3][ind - 1]),
+            (y4, diffs[4][ind - 2])]
+    intrpr4 = intrp3
+    p_t = ((intrpr4 - y - t*y1) / y1).subs(sub4)
+
+
+
+    print
+    print " Inverse interpolation "
+    print
+
+    ind = 6
+    sub4 = [(y0, diffs[0][ind]),
+            (y1, diffs[1][ind]),
+            (y2, diffs[2][ind - 1]),
+            (y3, diffs[3][ind - 1]),
+            (y4, diffs[4][ind - 2])]
+    intrpr4 = intrp3
+    p_t = ((intrpr4 - y - t*y1) / y1).subs(sub4)
+
+    tk   = 0
+    tk_1 = 0
+    eps = 1e-5
+    for k in range(0, 20):
+        tk_1 = p_t.subs(t, tk)
+        print " k  |       tk       |       p(tk)    "
+        print "---------------------------------------------------------------------------"
+        print " {}   |  {:+.9f}  |  {:+.9f}".format(k, float(tk), float(tk_1))
+        print "---------------------------------------------------------------------------"
+        if abs(tk_1 - tk) < eps:
+            break
+        tk = tk_1
+
+    tk = tk_1
+    x = h * tk + xs[ind]
+
+    p_x = intrpr4.subs(sub4)
+    p_x = p_x.subs(t, tk)
+
+    print ""
+    print "       y*       |       x*       |       p(x*)   "
+    print "---------------------------------------------------------------------------"
+    print "  {:+.9f}  |  {:+.9f}  |  {:+.9f}".format(float(y), float(x), float(p_x))
+    print "---------------------------------------------------------------------------"
+
