a207c1791647bba34bc064263d919415f192e0ca
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2015-03-24 22:02:24 +0300

    nekrasov method

--- task10.py
+++ task10.py
@@ -18,7 +18,19 @@ def print_iter(iter, x):
 def get_simple_approx(alpha, beta, x):
     return beta + alpha * x
 
-def simple_itr_method(a, b, eps, cb_print):
+def get_nekrasov_approx(alpha, beta, x):
+    n = x.shape[0]
+    xk = zeros(n)
+    for i in range(0, n):
+        for j in range(0, i-1):
+            xk[i] += alpha[i, j] * xk[j]
+        for j in range(i+1, n):
+            xk[i] += alpha[i,j]  * x[j]
+        xk[i] += beta[i]
+    return xk
+
+
+def iteration_method(a, b, eps, cb_approx, cb_print):
     nrow = a.shape[0]
     ncol = a.shape[1]
     alpha = zeros(nrow, ncol)
@@ -35,15 +47,15 @@ def simple_itr_method(a, b, eps, cb_print):
         beta[i] = b[i] / a[i, i]
 
     xk = beta
-    xk_1 = get_simple_approx(alpha, beta, xk)
+    xk_1 = cb_approx(alpha, beta, xk)
     cb_print(0, xk)
     cb_print(1, xk_1)
 
     iter = 1
     diff = [sys.float_info.max]
-    while max(diff) > eps and iter < 10:
+    while max(diff) > eps and iter < 20:
         xk = xk_1
-        xk_1 = get_simple_approx(alpha, beta, xk)
+        xk_1 = cb_approx(alpha, beta, xk)
         cb_print(iter, xk_1)
         diff = [abs(xi1 - xi2) for (xi1, xi2) in zip(xk, xk_1)]
         iter += 1
@@ -63,6 +75,9 @@ if __name__ == "__main__":
     pprint(solve_linear_system(system, x1, x2, x3))
     print ' '
 
-    simple_itr_method(a, b, 1e-5, print_iter)
+    print " Simple approximation: \n"
+    iteration_method(a, b, 1e-5, get_simple_approx, print_iter)
+    print "\n Nekrasov approximation: \n"
+    iteration_method(a, b, 1e-5, get_nekrasov_approx, print_iter)
 
     pass
